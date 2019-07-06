8ff2eef8cd69b60488565698d508b29aa154bd6f
Andy Bell <me@andy-bell.co.uk>
2019-07-06 13:11:00 +0100

    Increase the webmentions limit

--- src/_data/webmentions.js
+++ src/_data/webmentions.js
@@ -4,7 +4,7 @@ const domain = 'andy-bell.design';
 
 module.exports = async function webmentions() {
   return axios
-    .get(`https://webmention.io/api/mentions.jf2?domain=${domain}&token=${token}`)
+    .get(`https://webmention.io/api/mentions.jf2?domain=${domain}&token=${token}&per-page=999999999`)
     .then(({data}) => {
       return data.children || [];
     })
